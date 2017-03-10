bfb16421e918c3bed2ad3217b21b4cd79b312edd
nofelmahmood <nofelmehmood@gmail.com>
2017-03-10 11:33:10 +0500

    Add dataSource

---
+++ TvTime/FavoritesViewController/FavoritesItemsDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  FavoritesItemsDataSource.swift
+//  TvTime
+//
+//  Created by Nofel Mahmood on 10/03/2017.
+//  Copyright © 2017 Nineish. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesItemsDataSource: NSObject {
+
+}
