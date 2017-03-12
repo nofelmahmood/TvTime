f8f8a7cdd01cdb5b2bbc03b09cfee4d64f08dafc
nofelmahmood <nofelmehmood@gmail.com>
2017-03-12 18:37:10 +0500

    Add favorites view data source

--- TvTime/FavoritesViewController/FavoritesItemsDataSource.swift
+++ TvTime/FavoritesViewController/FavoritesItemsDataSource.swift
@@ -7,7 +7,36 @@
 //
 
 import UIKit
+import Alamofire
+import PromiseKit
+import AlamofireObjectMapper
+import AlamofireImage
 
 class FavoritesItemsDataSource: NSObject {
 
+    var items: [TvShow]?
+    
+    func prepare(items: [TvShow]?) {
+        self.items = items?.filter({ item in
+            return item.favorite
+        })
+    }
+}
+
+extension FavoritesItemsDataSource: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items == nil ? 0: items!.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ItemTableViewCell.self), for: indexPath) as! ItemTableViewCell
+        
+        let item = self.items![indexPath.row]
+        
+        cell.nameLabel.text = item.name
+        
+        return cell
+    }
 }
