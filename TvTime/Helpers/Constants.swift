65d193d44175849379f0a0871b1567de4c235f2d
nofelmahmood <nofelmehmood@gmail.com>
2017-03-12 16:13:20 +0500

    Add api and font constants

--- TvTime/Helpers/Constants.swift
+++ TvTime/Helpers/Constants.swift
@@ -18,9 +18,15 @@ struct APIEndPoint {
     static let popular = "https://api.themoviedb.org/3/tv/popular"
     static let rated = "https://api.themoviedb.org/3/tv/top_rated"
     static let today = "https://api.themoviedb.org/3/tv/airing_today"
+    static let externalIDs = "https://api.themoviedb.org/3/tv/1402/external_ids"
     static let image = "https://image.tmdb.org/t/p/w500"
+    static let imdb = "http://www.omdbapi.com/"
 }
 
 struct Color {
     static let silver = UIColor(colorLiteralRed: 192/255, green: 192/255, blue: 192/255, alpha: 1)
+}
+
+struct Font {
+    static let name = "Avenir-Light"
 }
