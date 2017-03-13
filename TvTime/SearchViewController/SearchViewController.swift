2822843923eec5ee2c5abb19fb1d7726e6e160df
nofelmahmood <nofelmehmood@gmail.com>
2017-03-13 12:10:08 +0500

    Add search

--- TvTime/SearchViewController/SearchViewController.swift
+++ TvTime/SearchViewController/SearchViewController.swift
@@ -9,8 +9,15 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
-    var searchController: UISearchController!
+    
+    lazy var tableView: UITableView = {
+        let tV = UITableView()
+        tV.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tV
+    }()
+    
+    let searchItemsDataSource = SearchItemsDataSource()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,12 +30,40 @@ class SearchViewController: UIViewController {
         navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: Color.silver]
         
         let searchBar = UISearchBar()
+        searchBar.delegate = self
         let searchBarTextField = searchBar.value(forKey: "searchField") as? UITextField
         searchBarTextField?.backgroundColor = UIColor.black
         searchBarTextField?.textColor = Color.silver
         searchBarTextField?.attributedPlaceholder = NSAttributedString(string: "Search Tv shows ...", attributes: [NSForegroundColorAttributeName: UIColor.darkGray])
         navigationItem.titleView = searchBar
         
+        view.addSubview(tableView)
+        
+        tableView.backgroundColor = UIColor.black
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 44.0
+        tableView.allowsSelection = false
+        tableView.separatorStyle = .none
+        
+        tableView.register(ItemTableViewCell.self, forCellReuseIdentifier: String(describing: ItemTableViewCell.self))
+        
+        tableView.pinLeadingToSuperview(margin: 8)
+        tableView.pinTrailingToSuperview(margin: 8)
+        tableView.pinTopToSuperview()
+        tableView.pinBottomToSuperview(margin: 8)
+        
+        tableView.dataSource = searchItemsDataSource
+        tableView.reloadData()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let searchBar = navigationItem.titleView
+        let searchBarTextField = searchBar?.value(forKey: "searchField") as? UITextField
+        searchBarTextField?.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,4 +82,20 @@ class SearchViewController: UIViewController {
     }
     */
 
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        guard let text = searchBar.text else {
+            return
+        }
+        
+        searchItemsDataSource.prepare(query: text, page: 1)
+            .then(execute: { (result) -> Void in
+                self.tableView.reloadData()
+        })
+        searchBar.resignFirstResponder()
+    }
 }
