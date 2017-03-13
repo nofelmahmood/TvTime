d200154bd47b9abf05e3a9eff91d44d488a04902
nofelmahmood <nofelmehmood@gmail.com>
2017-03-13 10:54:42 +0500

    Add model

---
+++ TvTime/Models/TvMazeTvShow.swift
@@ -0,0 +1,21 @@
+//
+//  TVMazeTvShow.swift
+//  TvTime
+//
+//  Created by Nofel Mahmood on 12/03/2017.
+//  Copyright © 2017 Nineish. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+
+
+struct TvMazeTvShow: Mappable {
+    
+    init?(map: Map) {}
+    
+    mutating func mapping(map: Map) {
+        
+        
+    }
+}
