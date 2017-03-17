a7904b6631813f01ed809deb3038b9569b7ff076
nofelmahmood <nofelmehmood@gmail.com>
2017-03-17 12:01:24 +0500

    Add Trakt API Constants

--- TvTime/Helpers/Constants.swift
+++ TvTime/Helpers/Constants.swift
@@ -11,6 +11,9 @@ import UIKit
 
 struct API {
     static let key = "b29ef964cc3c10d59b806d3e0c1947b9"
+    static let traktClientID = "784fd7633675d4b52c7d5732930a96c51482c440933174d0ffabc0aa978dba7c"
+    static let traktClientSecret = "18238e1289fa0db6f3e75771b63ba14f5b33968c6d71420bdb934c0d9ffdd29d"
+    static let redirectURI = "http://www.google.com"
 }
 
 struct APIEndPoint {
@@ -24,6 +27,9 @@ struct APIEndPoint {
     static let tv = "https://api.themoviedb.org/3/tv"
     static let search = "https://api.themoviedb.org/3/search/tv"
     static let imdb = "http://www.omdbapi.com/"
+    
+    static let traktAuthorize = "https://api.trakt.tv/oauth/authorize"
+    static let traktToken = "https://api.trakt.tv/oauth/token"
 }
 
 struct Color {
