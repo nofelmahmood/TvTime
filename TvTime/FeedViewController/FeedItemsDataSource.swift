b8428e6ce10fc138fc933f809eb140afa4f2484e
nofelmahmood <nofelmehmood@gmail.com>
2017-03-22 10:48:25 +0500

    Shift to collectionview datasource and delegate

--- TvTime/FeedViewController/FeedItemsDataSource.swift
+++ TvTime/FeedViewController/FeedItemsDataSource.swift
@@ -16,6 +16,7 @@ import RealmSwift
 
 protocol FeedItemsDataSourceDelegate {
     func feedItemsDataSource(dataSource: FeedItemsDataSource, onFavorite favorite: Bool)
+    func feedItemsDataSource(dataSource: FeedItemsDataSource, didSelectTvShow tvShow: TraktTvShow, andImage image: UIImage?)
 }
 
 class FeedItemsDataSource: NSObject {
@@ -24,21 +25,24 @@ class FeedItemsDataSource: NSObject {
     var items: [TraktTvShow]?
     var page = 1
     var imageURLs = [IndexPath: String]()
+    var favorited = [IndexPath]()
     
     var delegate: FeedItemsDataSourceDelegate?
     
     func prepare(forSegment segment: Int) -> AnyPromise {
         
+        page = 1
+        imageURLs = [IndexPath: String]()
         let promise = Promise<Any>(resolvers: { resolve, reject in
            
-            var itemsPromise = Trakt.shared.getTvShows(showsType: TraktTvShowsType.popular, page: 0)
+            var itemsPromise = Trakt.shared.getTvShows(showsType: TraktTvShowsType.popular, page: page)
             
             switch segment {
             case 1:
-                itemsPromise = Trakt.shared.getTvShows(showsType: TraktTvShowsType.trending, page: 0)
+                itemsPromise = Trakt.shared.getTvShows(showsType: TraktTvShowsType.trending, page: page)
                 break;
             case 2:
-                itemsPromise = Trakt.shared.getTvShows(showsType: TraktTvShowsType.anticipated, page: 0)
+                itemsPromise = Trakt.shared.getTvShows(showsType: TraktTvShowsType.anticipated, page: page)
                 break
             default:
                     break;
@@ -86,97 +90,9 @@ class FeedItemsDataSource: NSObject {
         return AnyPromise(promise)
     }
     
-    func getPopularItems(page: Int) -> AnyPromise {
-        
-        let urlString = "\(APIEndPoint.popular)?api_key=\(API.key)&language=en-US&page=\(page)"
-        let url = URL(string: urlString)!
-        
-        let promise = Promise<[TvShow]>(resolvers: { resolve, reject in
-            
-            Alamofire.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil).responseArray(keyPath: "results", completionHandler: { (response: DataResponse<[TvShow]>) in
-                
-                if response.result.isSuccess {
-                    resolve(response.result.value!)
-                    
-                } else {
-                    reject(response.error!)
-                }
-                
-            })
-        })
-        
-        return AnyPromise(promise)
-    }
-    
-    func getRatedItems(page: Int) -> AnyPromise {
-        
-        let urlString = "\(APIEndPoint.rated)?api_key=\(API.key)&language=en-US&page=\(page)"
-        let url = URL(string: urlString)!
-        
-        let promise = Promise<[TvShow]>(resolvers: { resolve, reject in
-            
-            Alamofire.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil).responseArray(keyPath: "results", completionHandler: { (response: DataResponse<[TvShow]>) in
-                
-                if response.result.isSuccess {
-                    resolve(response.result.value!)
-                    
-                } else {
-                    reject(response.error!)
-                }
-                
-            })
-        })
-        
-        return AnyPromise(promise)
-    }
-    
-    func getTodayItems(page: Int) -> AnyPromise {
-        
-        let urlString = "\(APIEndPoint.today)?api_key=\(API.key)&language=en-US&page=\(page)"
-        let url = URL(string: urlString)!
-        
-        let promise = Promise<[TvShow]>(resolvers: { resolve, reject in
-            
-            Alamofire.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil).responseArray(keyPath: "results", completionHandler: { (response: DataResponse<[TvShow]>) in
-                
-                if response.result.isSuccess {
-                    
-                    resolve(response.result.value!)
-                    
-                } else {
-                    reject(response.error!)
-                }
-                
-            })
-        })
-        
-        return AnyPromise(promise)
-    }
- 
-    func getTraktPopularShows() -> AnyPromise {
-        
-        let url = "\(APIEndPoint.traktPopular)?extended=full"
-        let headers = ["Content-Type": "application/json", "trakt-api-version": "2", "trakt-api-key": API.traktClientID]
-        
-        let promise = Promise<Any>(resolvers: { resolve, reject in
-            
-            Alamofire.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: headers).responseJSON(completionHandler: { response in
-                
-                if response.result.isSuccess {
-                    let json = response.result.value as! [[String: AnyObject]]
-                    let tvShows = try? [TraktTvShow].decode(json)
-                    print("Results are \(tvShows)")
-                    print("JSON is \(json)")
-                    resolve(json)
-                } else {
-                    reject(response.error!)
-                }
-            })
-        })
-        
-        return AnyPromise(promise)
-    }
 }
+
+// MARK: - UITableViewDataSource
 
 extension FeedItemsDataSource: UITableViewDataSource {
     
@@ -188,12 +104,13 @@ extension FeedItemsDataSource: UITableViewDataSource {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ItemTableViewCell.self), for: indexPath) as! ItemTableViewCell
         
-        var item = items![indexPath.row]
+        let item = items![indexPath.row]
         
         cell.setTvShow(tvShow: item, row: indexPath.row)
         cell.favorited = false
         
         cell.onFavorite = {
+            
            // self.items![cell.tag].favorite = !item.favorite
            // item.favorite = !item.favorite
            // cell.favorited = item.favorite
@@ -201,24 +118,87 @@ extension FeedItemsDataSource: UITableViewDataSource {
           //  self.delegate?.feedItemsDataSource(dataSource: self, onFavorite: item.favorite)
         }
         
+        guard let imdbID = item.imdbID else {
+            return cell
+        }
+        
         if imageURLs[indexPath] == nil {
-            Trakt.shared.getPosterURL(imdbID: item.imdbID)
+            cell.itemImageView.image = nil
+            Trakt.shared.getPosterURL(imdbID: imdbID)
                 .then(execute: { (result) -> Void in
-                    cell.itemImageView.image = nil
                     
                     let urlString = result as! String
                     let url = URL(string: urlString)!
                     self.imageURLs[indexPath] = urlString
+
                     cell.itemImageView.af_setImage(withURL: url)
             })
         } else {
             
+            cell.itemImageView.image = nil
             let imageURL = imageURLs[indexPath]!
             let url = URL(string: imageURL)!
-            cell.itemImageView.image = nil
             cell.itemImageView.af_setImage(withURL: url)
+
         }
         
         return cell
     }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension FeedItemsDataSource: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items == nil ? 0: items!.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let itemCell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: FeedItemCollectionViewCell.self), for: indexPath) as! FeedItemCollectionViewCell
+        
+        let tvShow = items![indexPath.row]
+        itemCell.setTvShow(tvShow: tvShow)
+        
+        itemCell.itemImageView.image = nil
+        
+        guard let imdbID = tvShow.imdbID else {
+            return itemCell
+        }
+        
+        if imageURLs[indexPath] == nil {
+            Trakt.shared.getPosterURL(imdbID: imdbID)
+                .then(execute: { (result) -> Void in
+                    
+                    let urlString = result as! String
+                    let url = URL(string: urlString)!
+                    self.imageURLs[indexPath] = urlString
+                    
+                    itemCell.itemImageView.af_setImage(withURL: url)
+                })
+        } else {
+            
+            let imageURL = imageURLs[indexPath]!
+            let url = URL(string: imageURL)!
+            itemCell.itemImageView.af_setImage(withURL: url)
+            
+        }
+        
+        return itemCell
+    }
+}
+
+// MARK: - UICollectionViewDelegate 
+
+extension FeedItemsDataSource: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let tvShow = items![indexPath.row]
+        let cell = collectionView.cellForItem(at: indexPath) as! FeedItemCollectionViewCell
+        let image = cell.itemImageView.image
+        
+        delegate?.feedItemsDataSource(dataSource: self, didSelectTvShow: tvShow, andImage: image)
+    }
+    
 }
