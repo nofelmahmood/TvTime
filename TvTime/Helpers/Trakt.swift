a4053ef81bc8d290053ba425c6a409778cd914c0
nofelmahmood <nofelmehmood@gmail.com>
2017-03-22 10:15:22 +0500

    Add new API Methods

--- TvTime/Helpers/Trakt.swift
+++ TvTime/Helpers/Trakt.swift
@@ -14,6 +14,7 @@ import PromiseKit
 class Trakt {
     
     static let shared = Trakt()
+    let limit = 200
     
     func authorize() -> AnyPromise {
         
@@ -77,7 +78,7 @@ class Trakt {
             break
         }
         
-        url = "\(url)?extended=full&page=\(page)&limit=20"
+        url = "\(url)?extended=full&page=\(page)&limit=\(limit)"
         
         let promise = Promise<[TraktTvShow]>(resolvers: { resolve, reject in
             Alamofire.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: headers).responseJSON(completionHandler: { response in
@@ -88,7 +89,100 @@ class Trakt {
                         json = json.map({ $0["show"] as! [String: AnyObject] })
                     }
                     let tvShows = try? [TraktTvShow].decode(json)
-                    resolve(tvShows!)
+                    
+                    if let tvShows = tvShows {
+                        resolve(tvShows)
+                    } else {
+                        let error = NSError(domain: "com.api.error", code: 0, userInfo: nil)
+                        reject(error)
+                    }
+                    
+                } else {
+                    reject(response.error!)
+                }
+            })
+        })
+        
+        return AnyPromise(promise)
+    }
+    
+    func getSeasons(slug: String) -> AnyPromise {
+        
+        let url = "\(APIEndPoint.traktShows)/\(slug)/seasons?extended=episodes"
+        let headers = ["Content-Type": "application/json", "trakt-api-version": "2", "trakt-api-key": API.traktClientID]
+    
+        let promise = Promise<[TraktSeason]>(resolvers: { resolve, reject in
+            
+            Alamofire.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: headers).responseJSON(completionHandler: { response in
+                
+                if response.result.isSuccess {
+                    
+                    let json = response.result.value as! [[String: AnyObject]]
+                    let seasons = try? [TraktSeason].decode(json)
+                    
+                    if let seasons = seasons {
+                        resolve(seasons)
+                    } else {
+                        let error = NSError(domain: "com.error.failed", code: 0, userInfo: nil)
+                        reject(error)
+                    }
+                } else {
+                    reject(response.error!)
+                }
+            })
+        })
+        
+        return AnyPromise(promise)
+    }
+    
+    func getEpisodeDetail(slug: String, seasonNumber: Int, episodeNumber: Int) -> AnyPromise {
+        
+        let url = "\(APIEndPoint.traktShows)/\(slug)/seasons/\(seasonNumber)/episodes/\(episodeNumber)?extended=full"
+        let headers = ["Content-Type": "application/json", "trakt-api-version": "2", "trakt-api-key": API.traktClientID]
+        
+        let promise = Promise<TraktEpisodeDetail>(resolvers: { resolve, reject in
+            
+            Alamofire.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: headers)
+                .responseJSON(completionHandler: { response in
+                    
+                    if response.result.isSuccess {
+                        let json = response.result.value as! [String: AnyObject]
+                        
+                        if let episodeDetail = try? TraktEpisodeDetail.decode(json) {
+                            resolve(episodeDetail)
+                            
+                        } else {
+                            let error = NSError(domain: "com.api.error", code: 0, userInfo: nil)
+                            reject(error)
+                        }
+
+                    } else {
+                        reject(response.error!)
+                    }
+                })
+        })
+        
+        return AnyPromise(promise)
+    }
+    
+    func getRelatedTvShows(slug: String) -> AnyPromise {
+        
+        let url = "\(APIEndPoint.traktShows)/\(slug)/related?extended=full"
+        
+        let promise = Promise<[TraktTvShow]>(resolvers: { resolve, reject in
+            
+            Alamofire.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil).responseJSON(completionHandler: { response in
+                
+                if response.result.isSuccess {
+                    let json = response.result.value as! [[String: AnyObject]]
+                    let tvShows = try? [TraktTvShow].decode(json)
+                    
+                    if let tvShows = tvShows {
+                        resolve(tvShows)
+                    } else {
+                        let error = NSError(domain: "com.api.error", code: 0, userInfo: nil)
+                        reject(error)
+                    }
                     
                 } else {
                     reject(response.error!)
@@ -117,5 +211,4 @@ class Trakt {
         
         return AnyPromise(promise)
     }
-    
 }
