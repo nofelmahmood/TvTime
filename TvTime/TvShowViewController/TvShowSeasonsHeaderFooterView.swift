4740897fd2176256cb6aeec9f201611de47b6fed
nofelmahmood <nofelmehmood@gmail.com>
2017-03-25 20:52:10 +0500

    Add TvShowSeasonsHeaderFooterView

---
+++ TvTime/TvShowViewController/TvShowSeasonsHeaderFooterView.swift
@@ -0,0 +1,21 @@
+//
+//  TvShowSeasonsHeaderFooterView.swift
+//  TvTime
+//
+//  Created by Nofel Mahmood on 25/03/2017.
+//  Copyright © 2017 Nineish. All rights reserved.
+//
+
+import UIKit
+
+class TvShowSeasonsHeaderFooterView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
