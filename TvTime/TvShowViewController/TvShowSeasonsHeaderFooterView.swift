a51eb5191c8ef70be352544c0b59b8038ba3608b
nofelmahmood <nofelmehmood@gmail.com>
2017-03-25 21:10:22 +0500

    Add layout elements

--- TvTime/TvShowViewController/TvShowSeasonsHeaderFooterView.swift
+++ TvTime/TvShowViewController/TvShowSeasonsHeaderFooterView.swift
@@ -9,7 +9,43 @@
 import UIKit
 
 class TvShowSeasonsHeaderFooterView: UITableViewHeaderFooterView {
-
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: Font.name, size: 16)
+        
+        return label
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let sV = UIStackView(arrangedSubviews: [self.titleLabel])
+        sV.axis = .vertical
+        sV.distribution = .fillProportionally
+        sV.spacing = 8
+        
+        return sV
+    }()
+    
+    lazy var mainView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = Color.cellBackground
+        view.layer.borderColor = Color.cellBorder.cgColor
+        view.layer.borderWidth = 1
+        
+        return view
+    }()
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        contentView.backgroundColor = Color.cellBackground
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -17,5 +53,7 @@ class TvShowSeasonsHeaderFooterView: UITableViewHeaderFooterView {
         // Drawing code
     }
     */
+    
+ 
 
 }
